b8cc96aa30b8eb7a369497fcb0dc942108d13b28
Bryan <bryan.dov.bergman@gmail.com>
2022-08-03 23:10:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mytablayoutapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mytablayoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout2);
+        TabItem tabChat = findViewById(R.id.chats);
+        TabItem tabCalls = findViewById(R.id.calls);
+        TabItem tabStatus = findViewById(R.id.status);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+    }
+}
